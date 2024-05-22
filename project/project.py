1127b1f61d877adc109cb119481cb39b8b932577
mhdi0mrdi <mhdi.mrd1383@gmail.com>
2024-05-22 18:31:24 +0330

    menu

---
+++ project/project.py
@@ -0,0 +1,23 @@
+
+from os import system
+
+
+student_list = []
+
+# region menu
+while True:
+    print("[A]dd student (1) :")
+    print("[S]how student (2) :")
+    print("[R]emove student (3) :")
+    print("[E]dit student (4) :")
+    print("[SE]srch student (5) :")
+    print("[AC]ctive student (6) :")
+    print("[DE]active student (7) :")
+    print("[PHP] best (8) :")
+    print("[PY]thon best (9) :")
+    print("[AVJ] best (10) :")
+    print("[EX]it (11) :")
+
+    menu = input("enter menu num :")
+    system("cls")
+# endregion
